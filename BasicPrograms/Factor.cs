371fbd7a672a704cd35cf662b35856ffdaf99793
“preetimca2016” <preetimca2016@gmail.com>
2022-08-10 18:00:30 +0530

    [Preeti] Add: UC1 for FlipCoin

---
+++ BasicPrograms/Factor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class Factor
+    {
+        public void Factors()
+        {
+            Console.WriteLine("Enter the number");
+            int Number = Convert.ToInt32(Console.ReadLine());   
+            for (
+                int i = 2; i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BasicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {              
+            Console.WriteLine("1: Flip coin");
+            Console.WriteLine("2: Leap Year");
+            Console.WriteLine("3: Power Of Two");
+            Console.WriteLine("4: Harmonic Function");
+            Console.WriteLine("5: Factors");
+            
+
+            int Answer = Convert.ToInt32(Console.ReadLine());
+            switch(Answer)
+            {            
+              
+                case 1:
+                    FlipCoinPercentage coin = new FlipCoinPercentage();
+                    coin.FlipCoin();
+                    break;
+                case 2:
+                    LeapYears leap = new LeapYears();
+                    leap.LeapYear();
+                    break;
+                case 3:
+                    powerOfTwo power = new powerOfTwo();
+                    power.PowerOfTwoNumbers();
+                    break;                
+                case 4:
+                    HarmonicNum harmonic = new HarmonicNum();
+                    harmonic.HarmonicFun();
+                    break;
+                case 5:
+                    Factor factor = new Factor();
+                    factor.Factors();
+                    break;
+               
+            }
+        }
+
+    }
+}
