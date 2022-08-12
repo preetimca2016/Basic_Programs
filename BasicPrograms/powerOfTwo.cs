072e64894dbd4508ef9a02883f3402845abf808d
“preetimca2016” <preetimca2016@gmail.com>
2022-08-13 03:18:21 +0530

    UC3-PowerofTwo

---
+++ BasicPrograms/powerOfTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class powerOfTwo
+    {
+        public void PowerOfTwoNumbers()
+        {
+            Console.WriteLine("Enter the Number: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            while (Number < 0 || Number >= 20)
+            {
+                Console.WriteLine("Invalid input ,plz enter the number");
+                Number = Convert.ToInt32(Console.ReadLine());
+            }
+            for( int i = 1; i <= Number; i++)
+            {
+                Console.WriteLine(Math.Pow(2,i));
+            }
+        }
+    }
+}
